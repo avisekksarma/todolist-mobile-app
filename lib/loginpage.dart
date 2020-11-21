d11166d7e928d601c054aa3d7e6a716cc67abbf3
avisekksarma <avisekksarma@gmail.com>
2020-11-21 16:46:35 +0545

    added app icon

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 // import 'package:http/http.dart' as http;
 import 'package:requests/requests.dart';
 
-
 class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,9 +16,9 @@ class LoginView extends StatelessWidget {
           padding: EdgeInsets.fromLTRB(50, 50, 50, 0),
           child: Column(children: [
             Container(
-              child: Text('Login to Get Started', style: TextStyle(fontSize: 30)),
-              margin:EdgeInsets.fromLTRB(0, 0, 0, 20)
-            ),
+                child: Text('Login to Get Started',
+                    style: TextStyle(fontSize: 30)),
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 20)),
             LoginForm(),
           ]),
         ));
@@ -32,7 +31,6 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
-
   final _formKey = GlobalKey<FormState>();
   final controllerUsername = TextEditingController();
   final controllerPassword = TextEditingController();
@@ -47,8 +45,6 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-
-
     return Container(
       height: 300,
       child: Form(
@@ -56,92 +52,88 @@ class _LoginFormState extends State<LoginForm> {
           child: Column(children: <Widget>[
             Card(
               child: TextFormField(
-                controller: controllerUsername,
-                decoration: InputDecoration(
-                  hintText:'Username'
-                ),
-                textAlign: TextAlign.center,
-                validator: (text) {
-                if (text.isEmpty) {
-                  return 'Please enter your username.';
-                }
-                return null;
-              }),
+                  controller: controllerUsername,
+                  decoration: InputDecoration(hintText: 'Username'),
+                  textAlign: TextAlign.center,
+                  validator: (text) {
+                    if (text.isEmpty) {
+                      return 'Please enter your username.';
+                    }
+                    return null;
+                  }),
             ),
             Card(
               child: TextFormField(
-                obscureText: true,
-                controller: controllerPassword,
-                decoration: InputDecoration(
-                  hintText:'Password'
-                ),
-                textAlign: TextAlign.center,
-                validator: (text) {
-                if (text.isEmpty) {
-                  return 'Please enter your password';
-                }
-                return null;
-              }),
+                  obscureText: true,
+                  controller: controllerPassword,
+                  decoration: InputDecoration(hintText: 'Password'),
+                  textAlign: TextAlign.center,
+                  validator: (text) {
+                    if (text.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  }),
             ),
             Padding(
                 padding: EdgeInsets.all(10),
                 child: RaisedButton(
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-                  color:Colors.orangeAccent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  color: Colors.orangeAccent,
                   child: Text('Login'),
                   onPressed: () {
                     if (_formKey.currentState.validate()) {
                       print('input is valid for login page.');
-                      loginViaApi(controllerUsername.text,controllerPassword.text);
+                      // loginViaApi(
+                      //     controllerUsername.text, controllerPassword.text);
                     }
                   },
                 )),
             Container(
-              margin:EdgeInsets.fromLTRB(0,10,0,0),
-              child: Row(
-                children: [
+                margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                child: Row(children: [
                   Padding(
-                    child: Text("Don't have an Account?"),
-                    padding:EdgeInsets.all(10)
-                  ),
+                      child: Text("Don't have an Account?"),
+                      padding: EdgeInsets.all(10)),
                   FlatButton(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-                    color:Colors.orange[200],
-                    child:Text('Register Here'),
-                    onPressed: (){
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    color: Colors.orange[200],
+                    child: Text('Register Here'),
+                    onPressed: () {
                       print('register here is clicked');
                       Navigator.pushReplacementNamed(context, 'register');
                     },
                   )
-                ])
-            )
+                ]))
           ])),
     );
   }
 }
 
-Future<void> loginViaApi(String username,String password) async{
-  http.Response response = await http.post(
-    'http://192.168.1.66:5000/api/login',
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode({
-      'username':username,
-      'password':password,
-      'app_name':'avisektodo'
-    })
-  );
-  if(response.statusCode == 200){
-    var x = json.decode(response.body);
-    print(x);
-    try{
-      print("this is ${x['token']}");
-    }catch(error){
-      print(error);
-    }
-  }else{
-    print(jsonDecode(response.body));
-  }
-  
-}
+// Future<void> loginViaApi(String username,String password) async{
+//   http.Response response = await http.post(
+//     'http://192.168.1.66:5000/api/login',
+//     headers: <String, String>{
+//       'Content-Type': 'application/json; charset=UTF-8',
+//     },
+//     body: jsonEncode({
+//       'username':username,
+//       'password':password,
+//       'app_name':'avisektodo'
+//     })
+//   );
+//   if(response.statusCode == 200){
+//     var x = json.decode(response.body);
+//     print(x);
+//     try{
+//       print("this is ${x['token']}");
+//     }catch(error){
+//       print(error);
+//     }
+//   }else{
+//     print(jsonDecode(response.body));
+//   }
+
+// }
