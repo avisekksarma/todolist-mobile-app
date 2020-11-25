7cca70b9f014665cb009f44388e47f11b6849693
avisekksarma <avisekksarma@gmail.com>
2020-11-25 19:14:12 +0545

    changed ipaddress from local to point to heroku rest api

--- lib/magic_number/magicnumbers.dart
+++ lib/magic_number/magicnumbers.dart
@@ -1,3 +1,4 @@
 // magic number of ip address
-String ipAddress = 'http://192.168.1.66:5000/';
+String ipAddress =
+    'https://flask-todolist-restapi.herokuapp.com/'; //  'http://192.168.1.66:5000/';
 String appName = 'avisektodo';
